ad2f97e7120ddd0b930f26c492252ebaed053a16
rolyPolyVole <yezhanting@gmail.com>
2023-06-14 16:22:11 +0100

    Added beginner tree project

---
+++ beginner/Tree.java
@@ -0,0 +1,21 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    //Introduce ourselves
+    System.out.println("Hey there, I'm rolyPolyVole, and I'm currently learning Java!");
+
+    //Project goal
+    System.out.println("Today I'm going to plant a tree using Java!'");
+
+    //Print a tree
+    System.out.println("   **  ");
+    System.out.println(" ****** ");
+    System.out.println("  **** ");
+    System.out.println("   \\/" );
+    System.out.println("   ||  ");
+
+    System.out.println("Yeah I know it's quite sad\nI thought this project was gonna be a bit more entertaining.");
+
+
+  }
+}
